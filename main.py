cb5ef22ac1355df0c8ee5c8698b2adcab66afbc3
5kamis52 <praneethk0040@gmail.com>
2021-12-02 23:18:19 +0530

    Changed project structure. Added a Main Window

---
+++ main.py
@@ -0,0 +1,3 @@
+import tkMain
+
+tkMain.showMain()
---
+++ tkMain.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+def showMain():
+    window=Tk()
+
+    window.title('Solent Campers')
+    window.geometry("500x400+400+150")
+    window.mainloop()
