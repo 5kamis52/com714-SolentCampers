460dfef3c580cc309d18318560b6f8392b3f3f4d
5kamis52 <praneethk0040@gmail.com>
2021-12-03 02:50:04 +0530

    Write Data to CSV

--- classBooking.py
+++ classBooking.py
@@ -1,8 +1,25 @@
+import os, csv
+
 class Booking:
-    def __init__(self, id, customerID, campID, camperID, bookingDate, campingDate):
+    def __init__(self, id, campID, camperID, regionID, bookingDate, campingDate):
         self.id = id
-        self.customerID = customerID
         self.campID = campID
         self.camperID = camperID
+        self.regionID = regionID
         self.bookingDate = bookingDate
         self.campingDate = campingDate
+
+    def writeBookingData(self):
+        header = ['id', 'camp', 'van', 'ragion', 'book_date', 'camp_date']
+
+        if os.path.isfile('data/bookings.csv'):
+            f = open('data/campers.csv', 'a', newline='', encoding='utf-8')
+            writer = csv.writer(f)
+            writer.writerow([self.id, self.campID, self.camperID, self.regionID, self.bookingDate, self.campingDate])
+            f.close()
+        else:
+            f = open('data/bookings.csv', 'w', newline='', encoding='utf-8')
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerow([self.id, self.campID, self.camperID, self.regionID, self.bookingDate, self.campingDate])
+            f.close()
