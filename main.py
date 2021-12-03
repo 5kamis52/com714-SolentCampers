f9fcd2f6f37e88c57d36bc4d9023521a2d9c06ee
5kamis52 <praneethk0040@gmail.com>
2021-12-03 08:54:47 +0530

    Add Data Dir of Not Exist

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import tkMain
+import os
 
+
+# create data directory if not exist
+if not os.path.exists('data'):
+    os.mkdir('data')
+
+#start main program
 mainWindow = tkMain.MainWindow()
 mainWindow.window.mainloop()
